78e867ee65cbac7504a3f3a6f457e6220437ecc4
Katherine Buechner <kbuechner@gmail.com>
2016-08-11 11:31:52 -0400

    pushing a readme and renaming the db to 'this-is-fine' instead of 'fsg'

--- server/env/development.js
+++ server/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
-  "DATABASE_URI": "postgres://localhost:5432/fsg",
-  "SESSION_SECRET": "Optimus Prime is my real dad",
+  "DATABASE_URI": "postgres://localhost:5432/this_is_fine",
+  "SESSION_SECRET": "We can do it if we try",
   "TWITTER": {
     "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
     "consumerSecret": "INSERT_TWITTER_CONSUMER_SECRET_HERE",
