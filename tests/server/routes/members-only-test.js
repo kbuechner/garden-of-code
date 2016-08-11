5ddbe0382a170d448fc4faa48593608f7b8c23ed
Stephanie Adams <adamsstephaniem@gmail.com>
2016-08-11 16:20:29 -0400

    user model & tests

--- tests/server/routes/members-only-test.js
+++ tests/server/routes/members-only-test.js
@@ -1,69 +1,69 @@
-// Instantiate all models
-var expect = require('chai').expect;
+// // Instantiate all models
+// var expect = require('chai').expect;
 
-var Sequelize = require('sequelize');
+// var Sequelize = require('sequelize');
 
-var db = require('../../../server/db');
+// var db = require('../../../server/db');
 
-var supertest = require('supertest');
+// var supertest = require('supertest');
 
-describe('Members Route', function () {
+// xdescribe('Members Route', function () {
 
-    var app, User;
+//     var app, User;
 
-    beforeEach('Sync DB', function () {
-        return db.sync({ force: true });
-    });
+//     beforeEach('Sync DB', function () {
+//         return db.sync({ force: true });
+//     });
 
-    beforeEach('Create app', function () {
-        app = require('../../../server/app')(db);
-        User = db.model('user');
-    });
+//     beforeEach('Create app', function () {
+//         app = require('../../../server/app')(db);
+//         User = db.model('user');
+//     });
 
-	describe('Unauthenticated request', function () {
+// 	describe('Unauthenticated request', function () {
 
-		var guestAgent;
+// 		var guestAgent;
 
-		beforeEach('Create guest agent', function () {
-			guestAgent = supertest.agent(app);
-		});
+// 		beforeEach('Create guest agent', function () {
+// 			guestAgent = supertest.agent(app);
+// 		});
 
-		it('should get a 401 response', function (done) {
-			guestAgent.get('/api/members/secret-stash')
-				.expect(401)
-				.end(done);
-		});
+// 		it('should get a 401 response', function (done) {
+// 			guestAgent.get('/api/members/secret-stash')
+// 				.expect(401)
+// 				.end(done);
+// 		});
 
-	});
+// 	});
 
-	describe('Authenticated request', function () {
+// 	describe('Authenticated request', function () {
 
-		var loggedInAgent;
+// 		var loggedInAgent;
 
-		var userInfo = {
-			email: 'joe@gmail.com',
-			password: 'shoopdawoop'
-		};
+// 		var userInfo = {
+// 			email: 'joe@gmail.com',
+// 			password: 'shoopdawoop'
+// 		};
 
-		beforeEach('Create a user', function (done) {
-			return User.create(userInfo).then(function (user) {
-                done();
-            }).catch(done);
-		});
+// 		beforeEach('Create a user', function (done) {
+// 			return User.create(userInfo).then(function (user) {
+//                 done();
+//             }).catch(done);
+// 		});
 
-		beforeEach('Create loggedIn user agent and authenticate', function (done) {
-			loggedInAgent = supertest.agent(app);
-			loggedInAgent.post('/login').send(userInfo).end(done);
-		});
+// 		beforeEach('Create loggedIn user agent and authenticate', function (done) {
+// 			loggedInAgent = supertest.agent(app);
+// 			loggedInAgent.post('/login').send(userInfo).end(done);
+// 		});
 
-		it('should get with 200 response and with an array as the body', function (done) {
-			loggedInAgent.get('/api/members/secret-stash').expect(200).end(function (err, response) {
-				if (err) return done(err);
-				expect(response.body).to.be.an('array');
-				done();
-			});
-		});
+// 		it('should get with 200 response and with an array as the body', function (done) {
+// 			loggedInAgent.get('/api/members/secret-stash').expect(200).end(function (err, response) {
+// 				if (err) return done(err);
+// 				expect(response.body).to.be.an('array');
+// 				done();
+// 			});
+// 		});
 
-	});
+// 	});
 
-});
+// });
