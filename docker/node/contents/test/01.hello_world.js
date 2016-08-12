6a2d74e67b640665aa486b09a8b1a458c88b95ce
Meredith Roman <meredith.c.roman@gmail.com>
2016-08-12 17:20:53 -0400

    added test

---
+++ docker/node/contents/test/01.hello_world.js
@@ -0,0 +1,19 @@
+const expect = require('chai').expect;
+
+eval(process.env.code);
+
+describe('Hello World', function () {
+	it('says hello', function () {
+		expect(something()).to.equal('Hello World');
+	});
+});
+
+// code="function something () {return 'Hello World';};" ./node_modules/mocha/bin/mocha --grep "Hello World"
+
+
+
+// docker run --rm --user=netuser --net=none --cap-drop all meredithroman/thisisfine:node node main.js --code="function something () {return 'Hello World';};"
+
+// docker run -t -i --rm --user=netuser --net=none --cap-drop all meredithroman/thisisfine:node bash
+
+// docker build -t meredithroman/thisisfine:node ./docker/node
