a9815f381f2d239b26c07f2fa57751874286b174
Meredith Roman <meredith.c.roman@gmail.com>
2016-08-14 18:26:09 -0400

    wrote hello world test

--- docker/node/contents/test/01.hello_world.js
+++ docker/node/contents/test/01.hello_world.js
@@ -1,19 +1,17 @@
 const expect = require('chai').expect;
+const stdout = require("test-console").stdout;
 
-eval(process.env.code);
+// challenge solution:
+// console.log('Hello World');
 
 describe('Hello World', function () {
-	it('says hello', function () {
-		expect(something()).to.equal('Hello World');
+
+	let output = stdout.inspectSync(function() {
+		eval(process.env.code);
 	});
+
+	it('says hello', function () {
+		expect(output[0]).to.equal('Hello World\n');
+	});
+
 });
-
-// code="function something () {return 'Hello World';};" ./node_modules/mocha/bin/mocha --grep "Hello World"
-
-
-// runs test on docker, passes test
-// docker run --rm --user=netuser --net=none --cap-drop all meredithroman/thisisfine:node node main.js --code="function something () {return 'Hello World';};"
-
-// docker run -t -i --rm --user=netuser --net=none --cap-drop all meredithroman/thisisfine:node bash
-
-// docker build -t meredithroman/thisisfine:node ./docker/node
