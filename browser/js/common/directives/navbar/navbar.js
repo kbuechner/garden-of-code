348d94de0975da8c6d44ee450a9882a11fcdc5ff
Dana Norwood <nimadera@nimadera.com>
2016-08-14 11:34:25 -0400

    nav items aligning correctly in navbar; not mobile-friendly; issues with selected class

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -9,8 +9,8 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state) 
             scope.items = [
                 { label: 'Home', state: 'home' },
                 { label: 'About', state: 'about' },
-                { label: 'Documentation', state: 'docs' },
-                { label: 'Members Only', state: 'membersOnly', auth: true }
+                { label: 'Paths', state: 'paths', auth: true},
+                { label: 'Documentation', state: 'docs' }
             ];
 
             scope.user = null;
