b52efe636ae6925439d82f755dc236a30c6942e9
Meredith Roman <meredith.c.roman@gmail.com>
2016-08-15 11:56:09 -0400

    slight formatting changes

--- server/app/routes/challenges/index.js
+++ server/app/routes/challenges/index.js
@@ -5,28 +5,17 @@ const util = require('util');
 const TIMEOUT_SECS = 5;
 const SUPPORTED_LANGUAGES = ['node']
 
-
-function postExecHandler(res, err, stdout) {
-	if (err) {
-		console.log('postExecHandler', util.inspect(err, {depth: null}));
-		var errTrace = {code: '', trace: [{'event': 'uncaught_exception'}]};
-		errTrace.trace[0].exception_msg = err.killed ? 'Timeout error. Your code ran for more than ' + TIMEOUT_SECS + ' seconds. Please shorten and try again.' : 'Unknown error. We apologize for the inconvenience.';
-		res.send(JSON.stringify(errTrace));
-	}
-	else {
-		res.send(stdout);
-	}
-}
+router.post('/:language/:id', runDocker);
 
 function runDocker(req, res) {
 	
-	let path = req.params.path;
-	if (!SUPPORTED_LANGUAGES.includes(path)) res.send(404);
+	let language = req.params.language;
+	if (!SUPPORTED_LANGUAGES.includes(language)) res.send(404);
 
 	let code = req.body.code;
 	let testId = req.params.id;
 	let exeFile = 'docker';
-	let imgName = "meredithroman/thisisfine:" + path
+	let imgName = "meredithroman/thisisfine:" + language
 	// this will only work for the Node container
 	// I will need to be more clever after I add Python
 	let args = ['run', '--rm', '--user=netuser', '--net=none', '--cap-drop', 'all', imgName, 
@@ -39,6 +28,16 @@ function runDocker(req, res) {
 
 }
 
-router.post('/:path/:id', runDocker);
+function postExecHandler(res, err, stdout) {
+	if (err) {
+		console.log('postExecHandler', util.inspect(err, {depth: null}));
+		var errTrace = {code: '', trace: [{'event': 'uncaught_exception'}]};
+		errTrace.trace[0].exception_msg = err.killed ? 'Timeout error. Your code ran for more than ' + TIMEOUT_SECS + ' seconds. Please shorten and try again.' : 'Unknown error. We apologize for the inconvenience.';
+		res.send(JSON.stringify(errTrace));
+	}
+	else {
+		res.send(stdout);
+	}
+}
 
 module.exports = router;
