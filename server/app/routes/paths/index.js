933db9e92ebcdc0a78c84d04053f45d7e140c873
Dana Norwood <nimadera@nimadera.com>
2016-08-19 13:01:55 -0400

    clean up

--- server/app/routes/paths/index.js
+++ server/app/routes/paths/index.js
@@ -11,17 +11,6 @@ router.get('/', function(req, res, next) {
 	.catch(next)
 });
 
-/*router.param('pathId', function (req, res, next, id) {
-	Path.findById(id)
-	.then(function (path) {
-		if (!path) res.status(404).send();
-		req.track = path; // req.path is already a thing so I'm confusingly calling a path a "track" here
-		next();
-		return null;
-	})
-	.catch(next);
-})*/
-
 router.get('/:pathId', function(req, res, next) {
 	let id = req.params.pathId;
 	Path.findById(id)
@@ -35,12 +24,6 @@ router.get('/:pathId', function(req, res, next) {
 });
 
 router.get('/:pathId/challenges', function (req, res, next){
-	/*req.track.allChallenges()
-	.then(function (challenges) {
-		res.json(challenges);
-	})
-	.catch(next);*/
-
 	Path.scope('allChallenges').findAll({
 		where: {
 			id: req.params.pathId
@@ -50,13 +33,6 @@ router.get('/:pathId/challenges', function (req, res, next){
 		res.send(path)
 	})
 	.catch(next);
-
-})
-
-// get all challenges by path
-	// api/paths/pathId (all of the challenges for the path)
-
-// get all paths
-	// api/paths/
+});
 
 module.exports = router;
