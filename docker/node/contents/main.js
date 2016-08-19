32014b79924e39c7385aa095c94de36cd264b1d4
Meredith Roman <meredith.c.roman@gmail.com>
2016-08-19 11:20:34 -0400

    docker api returns an object after tests are called

--- docker/node/contents/main.js
+++ docker/node/contents/main.js
@@ -15,7 +15,7 @@ let testId = argv.testId;
 let command = 'code=' + '"' + code + '"' + ' ./node_modules/mocha/bin/mocha --grep "' + TESTS[testId] +'"'
 
 child_process.exec(command, function (err, sdout, sderr) {
-	if (err) console.log(err);
-	if (sderr) console.log(sderr);
-	else console.log(sdout);
+	let passed = err ? false : true;
+	sdout = sdout.replace(/\n/g, "");
+	console.log('{"passed": ' + passed + ', "output": "' + sdout + '"}');
 });
