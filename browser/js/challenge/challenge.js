16accfe0ccb6cdb7a55fdc1356ec981722a63f33
Stephanie Adams <adamsstephaniem@gmail.com>
2016-08-20 18:58:45 -0400

    no merge conflicts here.

--- browser/js/challenge/challenge.js
+++ browser/js/challenge/challenge.js
@@ -14,7 +14,6 @@ app.controller('ChallengeCtrl', function ($scope, $stateParams, ChallengeFactory
 	.then(function (challenge) {
 		$scope.challenge = challenge;
 		$scope.runTests = function(code) {
-			console.log(challenge.language, challenge.id);
 			ChallengeFactory.runTests(challenge.language, challenge.id, code)
 			.then(function(result){
 				let results = result.output.replace(/\n/g, "<br />");
