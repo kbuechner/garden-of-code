84e440be99bc726836337cb50ec0d7cb2c2fec20
Katherine Buechner <kbuechner@gmail.com>
2016-08-22 11:54:06 -0400

    integrating plant view into page

---
+++ browser/js/plant/plant.js
@@ -0,0 +1,63 @@
+app.directive('plantDirective', function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'js/plant/plant.html'
+	}
+})
+
+// app.config(function ($stateProvider) {
+// 	$stateProvider.state('plant', {
+// 		url: '/challenges/:id',
+// 		templateUrl: 'js/challenge/challenge.html',
+// 		controller: 'PlantCtrl'
+//     });
+// });
+// app.controller('PlantCtrl', function ($scope, $stateParams, ChallengeFactory, $timeout){
+
+// 	let id = $stateParams.id;
+
+
+// 	ChallengeFactory.getChallenge(id)
+// 	.then(function (challenge) {
+// 		$scope.challenge = challenge;
+// 		$scope.runTests = function(code) {
+// 			ChallengeFactory.runTests(challenge.language, challenge.id, code)
+// 			.then(function(result){
+// 				$scope.results = result;
+// 			});
+// 		};
+// 		$scope.saveCode = function(code) {
+// 			ChallengeFactory.saveCode(challenge.id, code)
+// 			// .then(function(result){
+// 				$scope.saved = true;
+// 				$timeout(function () {$scope.saved = false}, 6000)
+// 			// });
+// 		}
+// 	});
+
+// });
+
+// app.factory('ChallengeFactory', function ($http) {
+
+// 	let factory = {};
+
+// 	factory.getChallenge = function (id) {
+// 		return $http.get('/api/challenges/' + id)
+// 		.then(function (resp) {
+// 			return resp.data;
+// 		});
+// 	}
+
+// 	factory.runTests = function (languageName, challengeId, challengeCode) {
+// 		return $http.post('/api/challenges/' + languageName + '/' + challengeId, {code: challengeCode})
+// 		.then(function (resp) {
+// 			// console.log(resp.data);
+// 			return resp.data;
+// 		});
+// 	}
+// 	factory.saveCode = function(challengeId,code) {
+// 		console.log("we'll save the code some other time!")
+// 		return 1
+// 	}
+// 	return factory;
+// });
---
+++ d3/test.js
@@ -0,0 +1,64 @@
+var margin = {top: 140, right: 10, bottom: 140, left: 10},
+    width = 240 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var orientations = {
+  "bottom-to-top": {
+    size: [width, height],
+    x: function(d) { return d.x; },
+    y: function(d) { return height - d.y; }
+  }
+};
+
+
+var svg = d3.select("body").selectAll("svg")
+    .data(d3.entries(orientations))
+  .enter().append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+d3.json("graph.json", function(error, root) {
+  if (error) throw error;
+
+  svg.each(function(orientation) {
+    var svg = d3.select(this),
+        o = orientation.value;
+
+    // Compute the layout.
+    var tree = d3.layout.tree().size(o.size),
+        nodes = tree.nodes(root),
+        links = tree.links(nodes);
+
+    // Create the link lines.
+    svg.selectAll(".link")
+        .data(links)
+      .enter().append("path")
+        .attr("class", "link")
+        .attr("d", d3.svg.diagonal().projection(function(d) { return [o.x(d), o.y(d)]; }));
+
+    // Create the node circles.
+
+    svg.selectAll(".node")
+        .data(nodes)
+        .enter().append("image")
+        .attr('xlink:href','resources/small_leaf.png')
+        .attr("height", function(d){ return d.leafsize})
+        .attr("width", function(d){ return d.leafsize})
+        .attr("x", o.x)
+        .attr("y", o.y)
+        .attr("transform", "translate(0, -10)");
+    svg.selectAll("text")
+        .data(nodes)
+        .enter()
+        .append("text")
+        .attr("x", o.x)
+        .attr("y", o.y)
+        .attr("transform", "translate(0, 20)")
+        .text(function(d){return d.text});
+  });
+});
+
