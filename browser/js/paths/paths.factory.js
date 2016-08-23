9f0e0e07ccacda6623411a9aafc563761bd4d4e5
Stephanie Adams <adamsstephaniem@gmail.com>
2016-08-23 19:27:20 -0400

    stuff

--- browser/js/paths/paths.factory.js
+++ browser/js/paths/paths.factory.js
@@ -2,18 +2,18 @@ app.factory('PathsFactory', function ($http) {
 	function getData(res) { return res.data};
 
     return {
-    	fetchAll: function() {
-    		return $http.get('/api/paths')
-    		.then(getData);
-    	},
-    	getChallenges: function(id) {
-    		var url = '/api/paths/' + id + '/challenges';
+	fetchAll: function() {
+		return $http.get('/api/paths')
+		.then(getData);
+	},
+	getChallenges: function(id) {
+		var url = '/api/paths/' + id + '/challenges';
 
-    		return $http.get(url)
-    		.then(function(res){
-    			return res.data[0];
-    		});
-    	}
-    }
+		return $http.get(url)
+		.then(function(res){
+			return res.data[0];
+		});
+	}
+}
 
 });
