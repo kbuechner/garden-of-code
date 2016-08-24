fa5bdced1f18b5494f6b304aaddd5ada8f22cb1c
Meredith Roman <meredith.c.roman@gmail.com>
2016-08-24 11:04:45 -0400

    redirect logged in users to dashboard state

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,18 @@
 app.config(function ($stateProvider) {
 	$stateProvider.state('home', {
 		url: '/',
-		templateUrl: 'js/home/home.html'
+		templateUrl: 'js/home/home.html',
+		controller: 'HomeCtrl',
+		resolve: {
+			user: function (AuthService) {
+				return AuthService.getLoggedInUser()
+			}
+		}
     });
+});
+
+app.controller('HomeCtrl', function ($state, user) {
+
+	if (user) $state.go('dashboard');
+
 });
