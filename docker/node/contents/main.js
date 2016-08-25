72015bffea85515ca9f266f93d5497f56c3e1cec
Meredith Roman <meredith.c.roman@gmail.com>
2016-08-25 14:11:57 -0400

    use json test reporting

--- docker/node/contents/main.js
+++ docker/node/contents/main.js
@@ -3,10 +3,8 @@ const child_process = require('child_process');
 let argv = require('minimist')(process.argv.slice(2));
 let code = argv.code;
 let testName = argv.testName;
-let command = 'code=' + '"' + code + '"' + ' ./node_modules/mocha/bin/mocha --grep "' + testName +'"'
+let command = 'code=' + '"' + code + '"' + ' ./node_modules/mocha/bin/mocha --grep "' + testName + '"' + ' --reporter json'
 
 child_process.exec(command, function (err, sdout, sderr) {
-	let passed = err ? false : true;
-	sdout = sdout.trim().replace(/\n/g, "\\n").replace(/'/g, "\\'").replace(/"/g, '\\"');
-	console.log('{"passed": ' + passed + ', "output": "' + sdout + '"}');
+	console.log(sdout);
 });
